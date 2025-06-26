b42f1682fe80bba595df455160638d46dc395862
Uday ahire <155221110+Uday5509@users.noreply.github.com>
2025-06-26 11:50:19 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,9 @@
+public class typecasting {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a;
+
+        System.out.println("Int: " + a);
+        System.out.println("double: " + b);
+    }
+}
